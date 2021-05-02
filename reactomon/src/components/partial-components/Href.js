5d9cfa8c7e34607bf04f68f4fb7c6262d3207325
Graner007 <graner.bence007@gmail.com>
2021-05-02 18:47:53 +0200

    replace consumer with useContext hook

--- reactomon/src/components/partial-components/Href.js
+++ reactomon/src/components/partial-components/Href.js
@@ -1,18 +1,15 @@
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../../contexts/ThemeContext';
+import { useContext } from "react";
 
 const Href = ({ url, text }) => {
-    return (
-        <ThemeContext.Consumer>{(context) => {
-            const { isLightTheme, light, dark } = context;
-            const theme = isLightTheme ? light : dark; 
-    
-            return (
-                <Link to={url} className="link" style={{ color: theme.syntax, background: theme.ui }}>{text}</Link>
-            );
-        }}
-        </ThemeContext.Consumer>
-    );
+
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+
+    return ( 
+        <Link to={url} className="link" style={{ color: theme.syntax, background: theme.ui }}>{text}</Link>
+    )
 }
 
 export default Href;
--- reactomon/src/components/pokemon-components/PokemonCard.js
+++ reactomon/src/components/pokemon-components/PokemonCard.js
@@ -1,25 +1,19 @@
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../../contexts/ThemeContext';
+import { useContext } from "react";
 
 const PokemonCard = ({ pokemon, index }) => {
 
     const id = pokemon.url.split("/")[6];
-
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark; 
+    
     return (
-        <ThemeContext.Consumer>{(context) => {
-            const { isLightTheme, light, dark } = context;
-            const theme = isLightTheme ? light : dark; 
-    
-            return (
-                <div className="card" key={ index } style={{ background: theme.bg, color: theme.syntax }}>
-                    {/* <img src={pokemon.url.sprites.frontDefault} alt="pokemon" style={{ width:'75px', height:'40px' }}/> */}
-                    <h3 style={{ color: theme.syntax }}>{ pokemon.name }</h3>
-                    <Link to={`/pokemon/${id}`} style={{ color: theme.syntax }}>{ id }</Link>
-                </div>
-            );
-        }}
-    
-        </ThemeContext.Consumer>
+        <div className="card" key={ index } style={{ background: theme.bg, color: theme.syntax }}>
+            {/* <img src={pokemon.url.sprites.frontDefault} alt="pokemon" style={{ width:'75px', height:'40px' }}/> */}
+            <h3 style={{ color: theme.syntax }}>{ pokemon.name }</h3>
+            <Link to={`/pokemon/${id}`} style={{ color: theme.syntax }}>{ id }</Link>
+        </div>
     );
 }
 
